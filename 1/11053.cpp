7cb713f31c8cb4e852e572c620974a4291b5fd9e
wja0 <dnwkdud99@gmail.com>
2022-02-11 13:41:01 +0900

    2022/02/11 Solve 2193, 14494

---
+++ 1/11053.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// 가장 긴 증가하는 부분 수열
+// LIS : Longest Increasing Subsequence
+// 수열 A에서 LIS 구하기
+// 수열 A = {10, 20, 10, 30, 20, 50}
+// 부분 수열 A = {10, 20, 30, 50}       길이 = 4
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    return 0;
+}
