2fbe6c929a774bc2bd2f769a1e3f07c917c4651f
wja0 <dnwkdud99@gmail.com>
2022-02-11 17:45:45 +0900

    2022/02/11 Solve 11053, 14002

--- 1/14002.cpp
+++ 1/14002.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <stack>
 using namespace std;
-// 가장 긴 증가하는 부분 수열
-// LIS : Longest Increasing Subsequence
-// 수열 A에서 LIS 구하기
-// 수열 A = {10, 20, 10, 30, 20, 50}
-// 부분 수열 A = {10, 20, 30, 50}       길이 = 4
-// D[i] : LIS의 길이
-// 추가되는 숫자의 앞에 올 수 있는 숫자들을 생각하면서 D[i] 값을 갱신
 // ex)   1   2   3   4
 // A[i]  10  20  10  30
 // D[i]  1 (10 앞에 숫자 없음)
@@ -16,8 +10,25 @@ using namespace std;
 //                    2 (30 앞에 10(3) 가능, 10의 D[i] +1)
 //                    3 (30 앞에 20 가능, 20의 D[i] +1) --> MAX로 선택
 //                    2 (30 앞에 10(1) 가능, 10의 D[i] +1)
+
+// 역 추적 필요
+// V[i] = A[i]의 앞에 와야하는 수의 인덱스.
+// 즉, A[i]의 앞에는 A[V[i]]가 와야 길이가 가장 길다
 int A[1001];
 int D[1001];
+int V[1001];
+int go(int i)
+{
+    if (i == -1)
+    {
+        return -1;
+    }
+    if (go(V[i]) != -1)
+    {
+        cout << A[V[i]] << " ";
+    }
+    return i;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -28,6 +39,7 @@ int main()
     {
         cin >> A[i];
         D[i] = 1;
+        V[i] = -1;
         // 전체 1로 초기화 반례 : 5 / 5 1 2 3 4
         for (int j = i - 1; j >= 0; j--)
         {
@@ -38,14 +50,22 @@ int main()
                 if (D[i] < tmp)
                 {
                     D[i] = tmp;
+                    V[i] = j;
                 }
             }
         }
     }
     // 가장 긴 LIS를 출력!!
-    sort(D, D + n);
-    cout << D[n - 1];
+    int max = 0, idx;
+    for (int i = 0; i < n; i++)
+    {
+        if (max < D[i])
+        {
+            max = D[i];
+            idx = i;
+        }
+    }
+    cout << max << '\n';
+    cout << A[go(idx)];
     return 0;
 }
-
-// O(N^2)
