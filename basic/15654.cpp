d91f153c20781a489d8bc57449ad064fdc32f1cb
wja0 <dnwkdud99@gmail.com>
2022-03-15 21:04:22 +0900

    Solve

---
+++ basic/15654.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+// 중복 없이 M개 고르기
+bool c[10];
+int a[10];
+int b[10];
+void go(int idx, int n, int m)
+{
+    if (idx == m)
+    {
+        int j = 0;
+        while (a[j] > 0)
+        {
+            cout << a[j] << " ";
+            j++;
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (c[i])
+            continue;
+        c[i] = true;
+        // 불가능하게 가둬두고 그 다음 숫자들 다시 뽑으러 함수 호출
+        if (a[idx - 1] != b[i])
+        {
+            a[idx] = b[i];
+            go(idx + 1, n, m);
+        }
+        // 다시 가능하게 열어주기
+        c[i] = false;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    int m;
+    cin >> n;
+    cin >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    sort(b, b + n);
+    go(0, n, m);
+
+    return 0;
+}
